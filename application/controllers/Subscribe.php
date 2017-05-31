0976552e2406ae08de1ada715b81f53bf91311fe
naanT <naan.developer@gmail.com>
2017-06-01 01:08:19 +0500

    Subscribe Controller

---
+++ application/controllers/Subscribe.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Subscribe extends CI_Controller {
+
+    public function index()
+    {
+        
+}
+
+/* End of file Subscribe.php */
